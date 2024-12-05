df5a2298f9013b31df8751f9caa35ff9eaa26c43
Axel Brisse <axel.brisse@gmail.com>
2024-12-05 11:05:36 +0100

    FenwickTree

---
+++ fenwick_tree.py
@@ -0,0 +1,47 @@
+import random
+import pytest
+
+
+class FenwickTree:
+    def __init__(self, data):
+        self.__len = len(data)
+        self.__tree = [0] * (self.__len + 1)
+        for i, n in enumerate(data, 1):
+            self.__tree[i] += n
+            j = i + (i & -i)
+            if j <= self.__len:
+                self.__tree[j] += self.__tree[i]
+
+    def update(self, i, delta):
+        i += 1
+        while i <= self.__len:
+            self.__tree[i] += delta
+            i += i & -i
+
+    def prefix_sum(self, i):
+        res = 0
+        while i > 0:
+            res += self.__tree[i]
+            i -= i & -i
+        return res
+
+    def range_sum(self, left, right):
+        return self.prefix_sum(right) - self.prefix_sum(left)
+
+    # TODO: nth_value using binary search
+
+
+@pytest.mark.parametrize("repeats", range(100))
+def test_fenwick_tree(repeats):
+    n = random.randint(1, 100)
+    a = [random.randint(-100, 100) for _ in range(n)]
+    fenwick_tree = FenwickTree(a)
+    for _ in range(random.randrange(100)):
+        i = random.randrange(n)
+        delta = random.randint(-100, 100)
+        fenwick_tree.update(i, delta)
+        a[i] += delta
+        for j in range(n):
+            assert fenwick_tree.prefix_sum(j) == sum(a[:j])
+        lo, hi = sorted(random.choices(range(n + 1), k=2))
+        assert fenwick_tree.range_sum(lo, hi) == sum(a[lo:hi])
